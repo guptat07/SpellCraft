765fb2716fe919422ff485406cbc3eb49e44195d
mwong365 <wong.michael.david@gmail.com>
2024-04-09 18:51:08 -0400

    Image API Implementation
    implemented image api utilizing google image searches

--- SpellCraft/Assets/WordConstructionScript.cs
+++ SpellCraft/Assets/WordConstructionScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Net.Http;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 
 public class WordConstructionScript : MonoBehaviour
@@ -12,10 +14,14 @@ public class WordConstructionScript : MonoBehaviour
     public GameObject welcomeTitle;
     public GameObject welcomeSubtitle;
     public TextMeshProUGUI definition;
+    public GameObject image;
+
 
     private static readonly HttpClient client = new HttpClient();
+    private static readonly HttpClient clientImage = new HttpClient();
 
     string previous = "";
+    string imagejson = "";
 
 
     // Start is called before the first frame update
@@ -26,7 +32,8 @@ public class WordConstructionScript : MonoBehaviour
         input.ActivateInputField();
         //gameObject.GetComponent<CanvasRenderer>().cull = false;
     }
-        public static string getBetween(string strSource, string strStart, string strEnd)
+    
+    public static string getBetween(string strSource, string strStart, string strEnd)
     {
         if (strSource.Contains(strStart) && strSource.Contains(strEnd))
         {
@@ -41,16 +48,63 @@ public class WordConstructionScript : MonoBehaviour
     async void call()
     {
         string url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + constructedWord.text;
+        //string url = "https://www.merriam-webster.com/dictionary/" + constructedWord.text;
+        //string url = "https://kids.wordsmyth.net/we/?ent=" + constructedWord.text;
         try
         {
             var response = await client.GetStringAsync(url);
-            definition.text = getBetween(response, "\"definition\":\"", ".\"");
+            Debug.Log(response);
+            string tempdef = getBetween(response, "\"definition\":\"", ",\"");
+            definition.text = tempdef.Substring(0,tempdef.Length-2);
+
         }
         catch (HttpRequestException e)
         {
             definition.text = "";
         }
     }
+    async void callimg() 
+    {
+
+        //string url2 = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBUzs5vWXhaZzXnfngxMlmL4SKEwxFNbvM&cx=91c356bcaa3b847d8&searchType=image&q=" + constructedWord.text;
+        string url2 = "https://www.google.com/search?tbm=isch&q=" + constructedWord.text;
+        // var response2 = await clientImage.GetStringAsync(url2);
+        // Debug.Log(response2);
+
+        try {
+            var response2 = await clientImage.GetStringAsync(url2);
+            imagejson = getBetween(response2, "alt=\"\" src=\"", "\"");
+            Debug.Log(imagejson);
+                StartCoroutine(createImage(imagejson));
+                                Debug.Log("step");
+        }
+        catch (HttpRequestException e) 
+        {
+
+        }
+    }
+
+    IEnumerator createImage(string tempurl)
+    {
+         Debug.Log("testing);");
+         var imagetemp = image.GetComponent<Image>();
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(tempurl);
+        Debug.Log("image gotten");
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+        {
+            Debug.LogWarning(request.error);
+            yield break;
+        }        
+        Debug.Log("createimage2");
+
+        var texture = DownloadHandlerTexture.GetContent(request);
+        var sprite1 = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), Vector2.one * 0.5f);
+        imagetemp.sprite = sprite1;
+        Debug.Log(imagetemp);
+        Debug.Log("sprite set");
+    }
+    
 
     // Update is called once per frame
     void Update()
@@ -63,9 +117,12 @@ public class WordConstructionScript : MonoBehaviour
             {
                 previous = constructedWord.text;
                 call();
+                callimg();
+
             }
             welcomeTitle.SetActive(false);
             welcomeSubtitle.SetActive(false);
+            Debug.Log("definition" + previous);
         }
 
 
