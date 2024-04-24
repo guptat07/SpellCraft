2c5e6066b7b6c2906a1160cc72f82488ddb1dc9a
Ian Kreger <iankreger@MacBook-Pro-175.local>
2024-04-24 01:14:01 -0400

    added scene and fixed blue color

--- SpellCraft/Assets/WordConstructionScript.cs
+++ SpellCraft/Assets/WordConstructionScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:834108662f7a180549093fcc2fb30a8f2afebb73a633f43d4e83aa033863deda
-size 12760
+oid sha256:2c363e2ef40bdd0b82d60e193c5b4c5d79b78736072db056daafe42e8daaef8d
+size 12817
